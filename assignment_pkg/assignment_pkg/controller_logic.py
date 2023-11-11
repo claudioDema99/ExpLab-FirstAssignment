871036928cad57e9b0b56413a190fa722d74ee5e
claudioDema99 <5433737@studenti.unige.it>
2023-11-11 17:45:40 +0100

    Update controller_logic.py

--- assignment_pkg/assignment_pkg/controller_logic.py
+++ assignment_pkg/assignment_pkg/controller_logic.py
@@ -32,19 +32,46 @@ class ControllerLogic(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         # INTERNAL VARIABLES
         self.distance = 0
+        self.id_marker = 0
+        # FLAG VARIABLES FOR EACH MARKER: when a marker is reached, set its flag to 1
+        self.m11 = 0
+        self.m12 = 0
+        self.m13 = 0
+        self.m15 = 0
 
     def timer_callback(self):
         msg = Twist()
-        if self.distance > 0.2:
-            msg.linear.x = 0.1
-        elif self.distance <= 0.2:
-            msg.linear.x = 0.0
+        if self.m11 == 0: # we are looking for the marker 11
+            if self.distance > 0.2 and self.id_marker == 11:
+                msg.linear.x = 0.1
+            elif self.distance <= 0.2 and self.id_marker == 11:
+                msg.linear.x = 0.0
+                self.m11 = 1
+        elif self.m11 == 1 and self.m12 == 0: # we are looking for the marker 12
+            if self.distance > 0.2 and self.id_marker == 12:
+                msg.linear.x = 0.1
+            elif self.distance <= 0.2 and self.id_marker == 12:
+                msg.linear.x = 0.0
+                self.m12 = 1
+        elif self.m12 == 1 and self.m13 == 0: # we are looking for the marker 13
+            if self.distance > 0.2 and self.id_marker == 13:
+                msg.linear.x = 0.1
+            elif self.distance <= 0.2 and self.id_marker == 13:
+                msg.linear.x = 0.0
+                self.m13 = 1
+        elif self.m13 == 1 and self.m15 == 0: # we are looking for the marker 15
+            if self.distance > 0.2 and self.id_marker == 15:
+                msg.linear.x = 0.1
+            elif self.distance <= 0.2 and self.id_marker == 15:
+                msg.linear.x = 0.0
+                self.m15 = 1
         self.publisher_.publish(msg)
 
     def aruco_callback(self, msg):
         #self.get_logger().info('I heard mid: "%s"' % msg.marker_ids[0])
         self.get_logger().info('POSITION: "%f", "%f", "%f"' % (msg.poses[0].position.x, msg.poses[0].position.y, msg.poses[0].position.z))
         self.get_logger().info('ORIENTATION: "%f", "%f", "%f", "%f"' % (msg.poses[0].orientation.x, msg.poses[0].orientation.y, msg.poses[0].orientation.z, msg.poses[0].orientation.w))
+        self.id_marker = msg.marker_ids[0]
         self.distance = msg.poses[0].position.z
 
     def odom_callback(self, msg):
