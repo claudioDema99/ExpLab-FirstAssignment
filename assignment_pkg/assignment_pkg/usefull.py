c416b3a30832505f07d8abf05b054d5e298b16a7
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-11-16 17:11:25 +0100

    ROTAZIONE  robot

--- assignment_pkg/assignment_pkg/usefull.py
+++ assignment_pkg/assignment_pkg/usefull.py
@@ -52,3 +52,43 @@ self.get_logger().info('Theta: "%f"' % (self.theta))
 # non ruota più e va sempre dritto per sempre
 # ma io devo arrivare AL marker guardandolo da li => devo forzarlo ad andare
 # a self.x > x_marker se son nel primo quadrante col marker...
+
+def timer_callback(self):
+        epsilon = 0.1
+        if self.theta < self.theta_gol + epsilon and self.theta > self.theta_gol - epsilon:
+            self.moving_to(self.x_gol)
+        else:
+            self.rotation(self.theta_gol)     
+        
+             
+    def stop_motor(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        self.publisher_.publish(msg)
+        
+    def rotation(self,theta_gol):
+        msg = Twist()
+        if theta_gol - 3.14 > 0: # controll to know if the shortest path is clockwise or unclockwise
+            if theta_gol - self.theta < 0.05: # rotate clockwise
+                msg.linear.x = 0.0
+                msg.angular.z = -0.5
+                self.publisher_.publish(msg)
+            else:
+                self.stop_motor()     
+        else:
+            if theta_gol - self.theta > 0.05: # rotate unclockwise
+                msg.linear.x = 0.0
+                msg.angular.z = 0.5
+                self.publisher_.publish(msg)
+            else:
+                self.stop_motor()    
+        
+    def moving_to(self, x_gol):
+        msg = Twist()
+        if x_gol - abs(self.x) > 0.1:
+            msg.linear.x = 0.1
+            msg.angular.z = 0.0
+            self.publisher_.publish(msg)
+        else:
+            self.stop_motor()
