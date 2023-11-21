189ab6a582c58e3a65116ae5b30f9c09052ca510
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-11-21 15:29:49 +0100

    Action client modifiche
    2 pub x la camera:
    - uno per la rotazione random con si o no
    - uno con la rotazione specifica per seguire il marker puntato
    
    aggiunta della logica da provare

--- assignment_pkg/assignment_pkg/robot_action_client.py
+++ assignment_pkg/assignment_pkg/robot_action_client.py
@@ -27,6 +27,8 @@ class CameraActionClient(Node):
         self.subscription_aruco  # prevent unused variable warning
         # PUBBLISHER TO CAMERA for rotating itself
         self.publisher_camera = self.create_publisher(bool, 'camera_on_off', 10)
+        # PUBBLISHER TO CAMERA the theta_goal
+        self.publisher_camera = self.create_publisher(float, 'camera_theta_goal', 10)
         # Initialize the camera or video feed
         self.cap = cv2.VideoCapture(0)  # Use 0 for default camera, update accordingly
         # timer for doing the controller logic
@@ -39,32 +41,66 @@ class CameraActionClient(Node):
         self.id_marker = 0
         self.goal_markers = [11, 12, 13, 15]
         self.marker_number = 0
+        self.flag = 0
 
-    # REQUEST to CONTROLLER to move to the goal position
-    # goal sender with (x_goal, y_goal)'s marker from the camera
-    def send_goal_position_marker(self, x_goal, y_goal):
+    ## TIMER for CONTROLLER LOGIC ##
+    def timer_callback(self):
+        if self.flag == 0:
+            self.rotation_camera_activation(True)
+            self.aruco_controller_area()
+        else:
+            self.position_marker_camera(self.theta)
+
+        
+    ## REQUEST to CONTROLLER to move to the goal position##
+    def send_goal_position_marker(self, x_goal, y_goal, theta):
         goal_msg = MarkerPosition.Goal()
         goal_msg.x_goal = x_goal
         goal_msg.y_goal = y_goal
+        goal_msg.theta_goal = theta
 
         self._action_client.wait_for_server()
 
         self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    # callback for checking if the info goal is reached
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    # result callback for goal reached 'reached'
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result.reached))
+        self.flag = 0
+
+    # feedback callback we aren't using it
+    #def feedback_callback(self, feedback_msg):
+    #    feedback = feedback_msg.feedback
+    #    self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
     
-    # REQUEST to CAMERA to rotate itself 
-    def send_goal_camera(self, theta):
-        goal_msg = Camera.Goal()
-        goal_msg.theta = theta
-
-        self._action_client.wait_for_server()
-
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-    # callback for UPDATE the ARUCO MARKER'S INFO
+    ## PUBLISHER to CAMERA for rotating itself ##
+    def rotation_camera_activation(self, on_off):
+        msg = bool()
+        msg.data = on_off
+        self.publisher_camera.publish(msg)
+        
+    # PUBLISHER to CAMERA the theta_goal
+    def position_marker_camera(self, theta):
+        msg = float()
+        msg.data = theta
+        self.publisher_camera.publish(msg)      
+        
+    ## callback for UPDATE the ARUCO MARKER'S INFO ##
     def aruco_callback(self, msg):
         # take the marker's id and position
         self.id_marker = msg.marker_ids[0]
@@ -96,8 +132,8 @@ class CameraActionClient(Node):
         # Detect markers in the image
         corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
-        # Check if marker 11 is detected
-        target_marker_id = 11
+        # Check if marker to detect is in the list of detected markers
+        target_marker_id = self.goal_markers[self.marker_number]
         
         if target_marker_id in ids:
             # Get the index of the target marker in the detected markers list
@@ -115,38 +151,21 @@ class CameraActionClient(Node):
             # Check if the marker area is inside the minimun area
             if min_area < marker_area:
                 # Marker is within the specified area
+                self.flag = 1
+                self.rotation_camera_activation(False)
+                self.position_marker_camera(self.theta)
+                self.send_goal_position_marker(self.x_goal, self.y_goal, self.theta)
+                self.marker_number += 1
                 print("Target marker is within the specified area.")
+            
             else:
                 print("Target marker is outside the specified area.")
 
         # Display the frame with detected markers
         frame_with_markers = aruco.drawDetectedMarkers(frame, corners)
         cv2.imshow("Frame with Markers", frame_with_markers)
-        cv2.waitKey(0)
+        cv2.waitKey(0)  
     
-    # callback for checking if the info goal is reached
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
-            return
-
-        self.get_logger().info('Goal accepted :)')
-
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    # result callback for goal reached 'reached'
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.reached))
-        rclpy.shutdown()
-
-    # feedback callback we aren't using it
-    def feedback_callback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
-
 
 def main(args=None):
     rclpy.init(args=args)
