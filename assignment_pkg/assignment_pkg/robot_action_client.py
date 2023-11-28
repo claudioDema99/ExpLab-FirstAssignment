4924d84edfceed6e71304d9cdd1f7f77ae749387
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-11-28 15:35:37 +0100

    Update robot_action_client.py
    bitbit e non quello di Willy il coyote

--- assignment_pkg/assignment_pkg/robot_action_client.py
+++ assignment_pkg/assignment_pkg/robot_action_client.py
@@ -1,22 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionClient
 from rclpy.executors import MultiThreadedExecutor
 from std_msgs.msg import Bool, Float64, Float32MultiArray
 
-from custom_action_interfaces_1explab.action import MarkerPosition
 from ros2_aruco_interfaces.msg import ArucoMarkers
 
 import math
 import numpy as np 
 
 
-class RobotActionClient(Node):
+class RobotControl(Node):
 
     def __init__(self):
-        super().__init__('robot_action_client')
-        # ACTION CLIENT to MOVE the ROBOT
-        self._action_client = ActionClient(self, MarkerPosition, 'marker_position')
+        super().__init__('robot_controller')
          # SUBSCRIBER TO ARUCO_MARKERS
         self.subscription_aruco = self.create_subscription(
             ArucoMarkers,
@@ -35,14 +31,12 @@ class RobotActionClient(Node):
         self.publisher_camera_onoff = self.create_publisher(Bool, 'camera_on_off', 10)
         # PUBBLISHER TO CAMERA the theta_goal
         self.publisher_camera_theta = self.create_publisher(Float64, 'camera_theta_goal', 10)  
+        # PUBBLISHER TO MOTOR for marker reached
+        self.publisher_marker_reached = self.create_publisher(Bool, 'marker_reached', 10)
         # TIMER for doing the controller logic
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)       
         ## INTERNAL VARIABLES ##
-        # goal marker's info
-        self.theta = 0
-        self.x_goal = 0
-        self.y_goal = 0
         self.id_marker = 0
         # marker's position in the list of markers seen by the camera
         self.position_marker = 0
@@ -80,71 +74,12 @@ class RobotActionClient(Node):
         elif self.flag == 1:
             #self.get_logger().info("Camera is following the marker, the robot is moving")
             self.aruco_follow_marker()           
-        # if the markers are reached go in home position
+
+        # end the program when all the markers are reached
         if self.reached_marker == 4:
-            self.get_logger().info("All markers are reached")
-            self.flag = 3
-            self.send_goal_position_marker(0, 0, 0)
-            self.rotation_camera_activation(False)
-            self.position_marker_camera(False) # camera in home position
-        # end the program
-        if self.reached_marker == 5:
-            self.get_logger().info("Work done!")
+            self.get_logger().info("All the markers are reached")
+            self.destroy_node()
             rclpy.shutdown()        
-                
-    ## ACTION CLIENT to MOVE the ROBOT ##
-    def send_goal_position_marker(self):
-        goal_msg = MarkerPosition.Goal()
-        goal_msg.x_goal = (self.x_goal + 0.1) # add 10 cm because it is the distance between the camera frame and the robot frame
-        goal_msg.y_goal = self.y_goal
-        theta = self.theta
-        # we need to convert the theta to respect the camera's frame into the marker's frame
-        if math.pi*1/8 < theta < math.pi*3/8:
-            theta = theta + math.pi
-        elif math.pi*3/8 < theta < math.pi*5/8:
-            theta = theta + math.pi
-            if theta > math.pi*2:
-                theta = theta - math.pi*2
-        elif math.pi*5/8 < theta < math.pi*7/8:
-            theta = theta - math.pi
-        elif theta < math.pi*1/8 or theta > math.pi*7/8:
-            theta = theta - math.pi
-            if theta < 0:
-                theta = theta + math.pi*2
-        goal_msg.theta_goal = theta
-        self.get_logger().info("Sending -> x_goal: {0}, y_goal: {1}, theta: {2}".format(goal_msg.x_goal, goal_msg.y_goal, goal_msg.theta_goal))
-        self._action_client.wait_for_server()
-        ###### DEBUG ######
-        self.wait_for_input()
-        ###################
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-    # goal callback for goal reached 'accepted'
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
-            return
-
-        self.get_logger().info('Goal accepted :)')
-
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    # result callback for goal reached 'succeeded'
-    def get_result_callback(self, future):
-        result = future.result().result
-        if result.reached == True: # if the goal is reached so I reacheved a TRUE value from the server
-            self.get_logger().info('Marker number {0} reached'.format(self.id_marker))
-            self.flag = 0
-            self.flag_marker = 0
-            self.reached_marker += 1
-
-    # feedback callback for goal reached 'feedback' but we don't use it
-    def feedback_callback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-        #self.get_logger().info('Distance to the marker goal: {0}'.format(feedback.distance_to_goal))
     
     ## PUBLISHER to CAMERA for rotating itself in searching mode ##
     def rotation_camera_activation(self, on_off):
@@ -154,14 +89,24 @@ class RobotActionClient(Node):
         
     ## PUBLISHER to CAMERA the theta_goal in following mode ##
     def position_marker_camera(self, active):
-        msg = Float64()
         if active == False: # if the marker is outside of the camera's field of view
-            theta = 0.1 # go near the home position
+            self.rotation_camera_activation(True)
         else:
+            msg = Float64()
             theta = self.theta
-        msg.data = theta
-        self.publisher_camera_theta.publish(msg)      
-        
+            msg.data = theta
+            self.publisher_camera_theta.publish(msg)
+    
+    ## PUBLISHER to MOTOR for marker reached ##
+    def marker_reached(self):
+        msg = Bool()
+        msg.data = True
+        self.publisher_marker_reached.publish(msg)
+        self.get_logger().info("Marker {0} reached".format(self.id_marker))
+        self.flag = 0
+        self.flag_marker = 0
+        self.reached_marker += 1       
+                   
     ## callback for UPDATE the MARKER'S INFO ##
     def aruco_callback(self, msg):
         self.id_marker = self.goal_markers[self.reached_marker] # take the marker's id to reach
@@ -183,9 +128,7 @@ class RobotActionClient(Node):
                 roll = math.pi + (math.pi + roll)  
             # take the marker's info   
             self.theta = roll
-            self.x_goal = msg.poses[self.position_marker].position.z
-            self.y_goal = msg.poses[self.position_marker].position.x
-            self.get_logger().info('x_goal: {0}, y_goal: {1}, theta: {2}'.format(self.x_goal, self.y_goal, self.theta))           
+            self.get_logger().info('Marker {0} found at position {1}'.format(self.id_marker, self.position_marker))         
         else:
             # if the marker is not in the list we wait and then we check again
             self.flag_marker = 0
@@ -220,7 +163,6 @@ class RobotActionClient(Node):
                 self.flag = 1
                 self.rotation_camera_activation(False)
                 self.position_marker_camera(True)
-                self.send_goal_position_marker()
             else:
                 self.get_logger().info("Target marker is outside.")
     
@@ -243,12 +185,17 @@ class RobotActionClient(Node):
         if self.flag_marker == 1 and len(self.corners_marker) != 0:                    
             # Calculate the area of the bounding box around the marker
             marker_area = self.calculate_rectangle_area(self.corners_marker)
-            # Define the minimum and maximum allowed area
-            min_area = 2000  # 20x20 pixels         
-            # Check if the marker area is inside the minimum area 
-            if marker_area < min_area:    
-                # Marker is within the specified area
-                self.position_marker_camera(True)
+            # define the area where the marker is close to the robot
+            area_distance = 4000 # 20x20 pixels  
+            if marker_area > area_distance:
+                self.marker_reached()
+                return
+            else:
+                min_area = 2000 # check if the marker is inside the camera's field of view    
+                # Check if the marker area is inside the minimum area 
+                if marker_area < min_area:    
+                    # Marker is within the specified area
+                    self.position_marker_camera(True)
         else:
             # Marker is outside of the camera's field of view
             self.position_marker_camera(False)
@@ -258,15 +205,15 @@ class RobotActionClient(Node):
 def main(args=None):
     rclpy.init(args=args)
     try:
-        action_client = RobotActionClient()
-        print("Robot Action Client node has been started")
+        robot_control = RobotControl()
+        print("Robot Control node has been started")
         
         # Create the executor
         ececuter = MultiThreadedExecutor()
         
         # Add the node to the executor
-        ececuter.add_node(action_client)
-        print("Robot Action Client node has been added to the executor")
+        ececuter.add_node(robot_control)
+        print("Robot Control node has been added to the executor")
         
         try:
             while rclpy.ok():
@@ -276,16 +223,16 @@ def main(args=None):
             # Destroy the node explicitly
             print('Destroying node...')
             ececuter.shutdown()
-            action_client.destroy_node()
+            robot_control.destroy_node()
             rclpy.shutdown()
             
     except KeyboardInterrupt:
         # Destroy the node explicitly
         print('Interrupted by user')
-        action_client.destroy_node()
+        robot_control.destroy_node()
         rclpy.shutdown()
 
-    rclpy.spin(action_client)
+    rclpy.spin(robot_control)
 
 
 if __name__ == '__main__':
