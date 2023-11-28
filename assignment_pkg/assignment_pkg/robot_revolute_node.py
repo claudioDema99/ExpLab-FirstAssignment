5dd68f092dacfedd2463ca7a177bc6a4fc1f0c24
Enrico Piacenti <enricopiacenti96@gmail.com>
2023-11-28 14:11:59 +0100

    Update robot_revolute_node.py

--- assignment_pkg/assignment_pkg/robot_revolute_node.py
+++ assignment_pkg/assignment_pkg/robot_revolute_node.py
@@ -20,6 +20,9 @@ class RobotController(Node):
         self.current_angle = Float64()
         self.current_angle.data = 0.0
 
+
+
+
         self.target = Bool()
         self.target.data = False  # True if a new target has been received
 
@@ -29,10 +32,12 @@ class RobotController(Node):
         self.dt = 0.1
 
         self.modality = Bool()
-        self.modality.data = False
+        self.modality.data = True # True if the camera is rotating, False if it is not rotating
         
         self.waypoint_reached = Bool()
-        self.waypoint_reached.data = False
+        self.waypoint_reached.data = True # True if the waypoint has been reached, False if it has not been reached yet
+
+
 
         self.sign = 1
 
@@ -41,7 +46,6 @@ class RobotController(Node):
         self.modality_timer = self.create_timer(self.dt, self.camera_modality) # Timer to check the modality of the camera, created at the beginning to avoid errors
 
 
-        self.theta_goal = 0.0  
 
         # Define PID controllers for linear velocity and rotation
         self.ang_pid = PID(0.5, 0.01, 0.1)
@@ -57,7 +61,7 @@ class RobotController(Node):
 
         self.subscriber_camera_modality = self.create_subscription(Bool, 'camera_on_off', self.camera_modality_callback, 10)
 
-        self.subscriber_camera_theta_goal = self.create_subscription(Float64, 'camera_theta_goal', self.camera_theta_callback, 10)
+        self.subscriber_camera_theta_goal = self.create_subscription(Float64, 'camera_theta_goal', self.camera_theta_callback, 100)
 
         # Cmd velocity publisher
         self.cmd_vel_pub = self.create_publisher(Float64MultiArray, "/joint_cam_controller/commands", 10)
@@ -71,12 +75,12 @@ class RobotController(Node):
     def camera_theta_callback(self, msg: Float64):
 
         #self.get_logger().info("Target.data is: {0})".format(self.target.data))
-        if self.ready.data == True:  # it is True if a goal target has been received, Useless, but I keep it for clarity
-            #self.get_logger().error('Received new target orientation (Theta: {0})'.format(msg.data))
-            # Store new target orientation (goal)
-            self.theta_goal = msg.data
-            self.target.data = True  # I don't need to receive another target yet
-            self.ang_pid.reset()
+        self.get_logger().error('Received new target orientation (Theta: {0})'.format(msg.data))
+        # Store new target orientation (goal)
+        self.theta_goal = msg.data
+        self.target.data = True  # I don't need to receive another target yet
+        self.ang_pid.reset()
+        self.waypoint_reached.data = False
 
 
     def camera_modality_callback(self, msg: Bool):
@@ -87,15 +91,17 @@ class RobotController(Node):
 
 
     # Control loop cycle callback
-    def camera_modality(self):
+    def camera_modality(self):  ## Questa funzione è chiamata ogni dt secondi, e controlla se la camera sta ruotando o no
 
-        #self.get_logger().info("Camera modality is: {0})".format(self.modality.data))
-        if self.modality.data == True: #Camera is rotating 
+        #self.get_logger().info("Searching Markers...") # solo questo ad inizo programma.
+
+
+        if self.modality.data == True: # Camera is rotatiing
+
+            
 
             self.waypoint_reached.data = True
 
-            #time.sleep(0.1)
-            # Incrementally increase the target angle
             self.current_angle.data += 0.01 * self.sign
 
             self.current_angle.data = round(self.current_angle.data, 3)
@@ -108,36 +114,32 @@ class RobotController(Node):
             cmd_msg = Float64MultiArray()
             cmd_msg.data = [self.current_angle.data]
             self.cmd_vel_pub.publish(cmd_msg)
-            #self.get_logger().info('(Sign: {0})'.format(self.sign))
-            #self.get_logger().info(' (Current_angleđ of rotation: {0})'.format(self.current_angle.data))
+            
 
+            self.get_logger().info("Camera is rotating, Current Angle is: {0})".format(self.current_angle.data))
+            #self.get_logger().info(' ( Waypoint_reached: {0})'.format(self.waypoint_reached.data))
+            #self.get_logger().info('(Sign: {0})'.format(self.sign))
 
             # Log the current angle
-            #self.get_logger().info("Camera is rotating...")
-
-        elif self.modality.data == False:
-
-            self.ready.data = True #Useless, but I keep it for clarity
-            self.waypoint_reached.data = False
-
-            if self.target.data == True:
-                self.ang_pid.sample_time = self.dt
-                self.ang_pid.setpoint = self.theta_goal
-                # Reset PID internals to clear previous errors
-                #self.ang_pid.reset()
-                #self.get_logger().warn("Target selected, keeping an eye on it..")
-                self.target.data = False
-                self.timer = self.create_timer(self.dt, self.control_loop_callback)
             
-            else:
-                print('Searching...')
+
+        elif self.modality.data == False : # Camera is not rotating and waypoint has not been reached yet
+
+
+            print('I am waiting for a new target, I am not rotating')
+            self.ang_pid.sample_time = self.dt
+            self.ang_pid.setpoint = self.theta_goal
+            self.target.data = False
+            self.timer = self.create_timer(self.dt, self.control_loop_callback)
+            
+            #else:
+            #    print('I am waiting for another ...')
                 #self.get_logger().error("Camera has stopped its rotation but target is not selected yet, waiting for a target..")
                 # Per un problema probabilmente di diverse sequenze di esecuzione tra programma e timer, ogni tanto il programma stampa
                 # questo messaggio anche se il target è stato selezionato, ma non è un problema perchè il programma funziona lo stesso
 
         else:
-            #self.get_logger().error("Something went wrong with the camera modality" )
-            print()
+            self.get_logger().info("Something went wrong with the camera modality" )
             
             
             
@@ -146,8 +148,9 @@ class RobotController(Node):
     # Control loop cycle callback, it is called every dt seconds and it works if waypoint_reached is False
     def control_loop_callback(self):
 
+
         if self.waypoint_reached.data == True:
-            #self.get_logger().error("Waypoint reached, waiting for a new target..")
+            #self.get_logger().error("Waypoint reached is True, waiting for a new target..")
             return
         
         # Compute remaining errors
@@ -155,11 +158,12 @@ class RobotController(Node):
 
 
         ## Debug
-        print()
-        print("inizio calcolo PID")
-        #self.get_logger().info("Current Angle: {0})".format(self.current_angle.data))
-        #self.get_logger().info("Theta goal: {0})".format(self.ang_pid.setpoint))
-        #self.get_logger().info("Remaining error: {0})".format(ang_error))
+        log_message = (
+            f"Current Angle: {self.current_angle.data}\n"
+            f"Theta goal: {self.ang_pid.setpoint}\n"
+            f"Remaining error: {ang_error}\n"
+        )
+        self.get_logger().info(log_message)
 
 
        
@@ -173,8 +177,8 @@ class RobotController(Node):
             self.current_angle.data = 6.27
             self.get_logger().error('Angle is too much')
             
-        elif self.current_angle.data < 0.1:
-            self.current_angle.data = 0.1
+        elif self.current_angle.data <= 0.01:
+            self.current_angle.data = 0.01
             self.get_logger().error('Angle is too low')
             
         self.current_angle.data += ang_control
@@ -186,13 +190,13 @@ class RobotController(Node):
 
         # If pose reached, exit the loop
         if ang_error < self.ang_threshold:
-            self.get_logger().info('Waypoint reached')
+            log_message += 'Waypoint reached\n'
+            self.get_logger().info(log_message)            
             self.waypoint_reached.data = True
             return
             
             
 
-    
 
 
 def main(args=None):
